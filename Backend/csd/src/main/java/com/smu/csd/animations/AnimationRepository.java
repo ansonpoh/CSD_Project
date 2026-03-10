7763270078eb982910257dcefb1a8848db1aefaa
Anson <ansonpoh0909@gmail.com>
2026-03-10 18:46:01 +0800

    removed animations

--- Backend/csd/src/main/java/com/smu/csd/animations/AnimationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smu.csd.animations;
-
-import java.util.UUID;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnimationRepository extends JpaRepository<Animation, UUID> {
-
-}

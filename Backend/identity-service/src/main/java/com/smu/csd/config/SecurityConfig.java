ac4028e759badeffc8cc8dfe0099768e6d263dac
Anson <ansonpoh0909@gmail.com>
2026-03-15 18:10:07 +0800

    updates

--- Backend/identity-service/src/main/java/com/smu/csd/config/SecurityConfig.java
+++ Backend/identity-service/src/main/java/com/smu/csd/config/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig {
                 .requestMatchers("/swagger-ui.html").permitAll()
                 .requestMatchers("/v3/api-docs/**").permitAll()
                 .requestMatchers("/webjars/**").permitAll()
+                .requestMatchers("/api/auth/role/internal/**").permitAll()
                 .anyRequest().authenticated()
             )
             .oauth2ResourceServer(oauth2 -> oauth2
